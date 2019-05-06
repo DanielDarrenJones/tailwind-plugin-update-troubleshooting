d579bace3978af87f88008ef3c737ea0980c73be
Adam Wathan <adam.wathan@gmail.com>
2019-05-06 13:03:45 -0400

    Add Daniel's config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,48 @@
+const postcss = require('postcss')
+
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        turnip: '#9f2074',
-      }
-    }
-  },
-  variants: {},
-  plugins: []
+    plugins: [
+        function({ addVariant, e }) {
+            addVariant('pointer-coarse', ({ container, separator }) => {
+                const pointerCoarse = postcss.atRule({ name: 'media', params: '(pointer: coarse)' })
+                pointerCoarse.nodes = container.nodes
+                container.nodes = [pointerCoarse]
+                pointerCoarse.walkRules(rule => {
+                    rule.selector = `.${e(`pointer-coarse${separator}${rule.selector.slice(1)}`)}`
+                })
+            })
+            addVariant('pointer-fine', ({ container, separator }) => {
+                const pointerFine = postcss.atRule({ name: 'media', params: '(pointer: fine)' })
+                pointerFine.nodes = container.nodes
+                container.nodes = [pointerFine]
+                pointerFine.walkRules(rule => {
+                    rule.selector = `.${e(`pointer-fine${separator}${rule.selector.slice(1)}`)}`
+                })
+            })
+            addVariant('pointer-none', ({ container, separator }) => {
+                const pointerNone = postcss.atRule({ name: 'media', params: '(pointer: none)' })
+                pointerNone.nodes = container.nodes
+                container.nodes = [pointerNone]
+                pointerNone.walkRules(rule => {
+                    rule.selector = `.${e(`pointer-none${separator}${rule.selector.slice(1)}`)}`
+                })
+            })
+            addVariant('hover-hover', ({ container, separator }) => {
+                const hoverHover = postcss.atRule({ name: 'media', params: '(hover: hover)' })
+                hoverHover.nodes = container.nodes
+                container.nodes = [hoverHover]
+                hoverHover.walkRules(rule => {
+                    rule.selector = `.${e(`hover-hover${separator}${rule.selector.slice(1)}`)}`
+                })
+            })
+            addVariant('hover-none', ({ container, separator }) => {
+                const hoverNone = postcss.atRule({ name: 'media', params: '(hover: none)' })
+                hoverNone.nodes = container.nodes
+                container.nodes = [hoverNone]
+                hoverNone.walkRules(rule => {
+                    rule.selector = `.${e(`hover-none${separator}${rule.selector.slice(1)}`)}`
+                })
+            })
+        }
+    ]
 }
